7677bec2fe36da209087d7e3d65c4c50642d1126
gauri <gauri.ramesh15@gmail.com>
2018-02-24 23:32:11 -0600

    Added some words

--- matchedWords.js
+++ matchedWords.js
@@ -2,18 +2,90 @@ var WORDS = {
     words: [
         {
             keyword: "just",
-            message: "Don't say just",
+            message: "Saying just underplays your message. Double click to remove",
             replacement: ""
         },
         {
             keyword: "sorry",
-            message: "You don't need to apologize",
-            replacement: "apologetic"
+            message: "What are you sorry for? Double click to remove.",
+            replacement: ""
         },
         {
             keyword: "i'm not sure",
-            message: "You are sure",
-            replacement: "YASS"
-        }
+            message: "Are you sure you're not sure? Double click to remove.",
+            replacement: ""
+        },
+        {
+            keyword: "maybe",
+            message: "Is it or is it not? That is the question... Double click to remove.",
+            replacement: ""
+        },
+        {
+            keyword: "may not be right",
+            message: "You are a rockstar who is most certainly right! Double click to remove.",
+            replacement: ""
+        },
+        {
+            keyword: "may not be correct",
+            message: "You are a rockstar who is most certainly right! Double click to remove.",
+            replacement: ""
+        },
+        {
+            keyword: "might not be correct",
+            message: "You are a rockstar who is most certainly right! Double click to remove.",
+            replacement: ""
+        },
+        {
+            keyword: "may be wrong",
+            message: "You are most certainly right! Double click to remove.",
+            replacement: ""
+        },
+        {
+            keyword: "i'm not sure if i understand",
+            message: "I'm sure you do understand. Double click to remove",
+            replacement: ""
+        },
+        {
+            keyword: "i'm not sure if i'm understanding",
+            message: "I'm sure you do understand. Double click to remove.",
+            replacement: ""
+        },
+        {
+            keyword: "hey",
+            message: "Hi there! Don't be too casual. Double click to change to 'hello'",
+            replacement: "hello"
+        },
+        {
+            keyword: "if that is ok",
+            message: "Do you really need to be asking for permission? Double click to remove.",
+            replacement: ""
+        },
+        {
+            keyword: "if that is okay",
+            message: "Do you really need to be asking for permission? Double click to remove.",
+            replacement: ""
+        },
+        {
+            keyword: "if that's ok",
+            message: "Do you really need to be asking for permission? Double click to remove.",
+            replacement: ""
+        },
+        {
+            keyword: "if that's okay",
+            message: "Do you really need to be asking for permission? Double click to remove.",
+            replacement: ""
+        },
+        {
+            keyword: "if thats ok",
+            message: "Do you really need to be asking for permission? Double click to remove.",
+            replacement: ""
+        },
+        {
+            keyword: "if thats okay",
+            message: "Do you really need to be asking for permission? Double click to remove.",
+            replacement: ""
+        },
+
+
     ],
 };

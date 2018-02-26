1190d3aa490dc023871c42940ac5b5244f415114
AllisonInman <ainman514@hotmail.com>
2018-02-25 23:50:55 -0600

    Update manifest to only load content scripts on the correct pages. Add message to Rosie that alerts a user when they try to use Rosie on a page we don't support

--- popup.js
+++ popup.js
@@ -1,5 +1,9 @@
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+	if(response == undefined){
+		$("#popupMessage").html("Rosie is compatible with Gmail, Outlook, Slack, LinkedIn, Yahoo, and GroupMe.");
+		return;
+	}
     $("#popupMessage").html(prepareForDisplay(response.messageText));
     var replacements = keywordMatching();
     registerKeywordListeners(replacements);

304d143b49ffe30a8cadd7b3f80477f4b2bf17c7
gauri <gauri.ramesh15@gmail.com>
2018-02-27 00:07:32 -0600

    Added question mark with instructions tooltip

--- popup.js
+++ popup.js
@@ -1,13 +1,13 @@
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-    $("#popupMessage").html(prepareForDisplay(response.messageText));
-    var replacements = keywordMatching();
-    registerKeywordListeners(replacements);
-	
-	new Clipboard('#copyBtn', {
-		text: function(trigger){
-			return prepareForCopy($("#popupMessage").html().toString());
-		}
-	});
+        $("#popupMessage").html(prepareForDisplay(response.messageText));
+        var replacements = keywordMatching();
+        registerKeywordListeners(replacements);
+        
+        new Clipboard('#copyBtn', {
+            text: function(trigger){
+                return prepareForCopy($("#popupMessage").html().toString());
+            }
+        });
     });
 });
